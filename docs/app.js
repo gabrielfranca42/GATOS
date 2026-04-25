833a351e7296f740fa24a91fcb382db69a7400f2
gabrielfranca42 <gabrielfranca172@gmail.com>
2026-04-25 11:05:52 -0300

    pepega

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-const API = 'https://gatos-46yy.onrender.com/dogs';
+const API = 'http://localhost:3000/dogs';
 
 const form = document.getElementById('form');
 const list = document.getElementById('list');
@@ -15,7 +15,6 @@ form.addEventListener('submit', async (e) => {
     body: formData
   });
 
-  form.reset();
   loadDogs();
 });
 
@@ -31,7 +30,7 @@ async function loadDogs() {
 
     div.innerHTML = `
       <h3>${dog.name}</h3>
-      <img src="https://gatos-46yy.onrender.com/uploads/${dog.image}" />
+      <img src="http://localhost:3000/uploads/${dog.image}" />
       <br>
       <button onclick="deleteDog('${dog._id}')">Excluir</button>
     `;
