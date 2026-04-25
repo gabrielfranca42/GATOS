9c4f48c89b0a897037ecc54c5e1f68062e1a705e
gabrielfranca42 <gabrielfranca172@gmail.com>
2026-04-25 09:11:52 -0300

    au au

---
+++ frontend/app.js
@@ -0,0 +1,47 @@
+const API = 'http://localhost:3000/dogs';
+
+const form = document.getElementById('form');
+const list = document.getElementById('list');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData();
+  formData.append('name', document.getElementById('name').value);
+  formData.append('image', document.getElementById('image').files[0]);
+
+  await fetch(API, {
+    method: 'POST',
+    body: formData
+  });
+
+  loadDogs();
+});
+
+async function loadDogs() {
+  const res = await fetch(API);
+  const dogs = await res.json();
+
+  list.innerHTML = '';
+
+  dogs.forEach(dog => {
+    const div = document.createElement('div');
+    div.className = 'card';
+
+    div.innerHTML = `
+      <h3>${dog.name}</h3>
+      <img src="http://localhost:3000/uploads/${dog.image}" />
+      <br>
+      <button onclick="deleteDog('${dog._id}')">Excluir</button>
+    `;
+
+    list.appendChild(div);
+  });
+}
+
+async function deleteDog(id) {
+  await fetch(`${API}/${id}`, { method: 'DELETE' });
+  loadDogs();
+}
+
+loadDogs();
